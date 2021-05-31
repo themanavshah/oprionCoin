5eaa9a026e8eabab5880ae34439bf9b9506c64c8
themanavshah <shahmanav765@gmail.com>
2021-05-31 10:45:53 +0530

    adding crowdsale with openzepplin 2.5.1 because not avalibale in x4.0

--- contracts/OprionToken.sol
+++ contracts/OprionToken.sol
@@ -1,12 +1,16 @@
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.0;
+pragma solidity 0.5.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract OprionToken is ERC721 {
-    constructor(string memory _name, string memory _symbol)
-        public
-        ERC721(_name, _symbol)
-    {}
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract OprionToken is ERC20Detailed, ERC20Pausable, ERC20Mintable {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) public ERC20Detailed(_name, _symbol, _decimals) {}
 }
